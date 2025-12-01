3d5b801e59f9b9357bc3ccc434cedb2deccc1b66
Matthäus Khandare <khandare.matthaeus@proton.me>
2025-12-01 06:40:10 +0100

    day 1

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,12 +1,56 @@
 defmodule Day01 do
   def part1(input) do
+    initial = {50, 0}
+
     input
-    |> IO.puts()
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(initial, fn line, {pos, count} ->
+      {direction, amount_str} = String.split_at(line, 1)
+
+      amount = String.to_integer(amount_str)
+      new_pos = calculate_position(pos, direction, amount)
+
+      new_count = if new_pos == 0, do: count + 1, else: count
+
+      {new_pos, new_count}
+    end)
+    |> elem(1)
   end
 
   def part2(input) do
+    initial = {50, 0}
+
     input
-    |> IO.puts()
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(initial, fn line, {pos, count} ->
+      {direction, amount_str} = String.split_at(line, 1)
+      amount = String.to_integer(amount_str)
+
+      rotate(pos, count, direction, amount)
+    end)
+    |> elem(1)
+  end
+
+  defp calculate_position(current, "L", amount) do
+    Integer.mod(current - amount, 100)
+  end
+
+  defp calculate_position(current, "R", amount) do
+    Integer.mod(current + amount, 100)
+  end
+
+  defp rotate(start_pos, start_count, direction, amount) do
+    Enum.reduce(1..amount, {start_pos, start_count}, fn _, {pos, count} ->
+      new_pos =
+        case direction do
+          "L" -> Integer.mod(pos - 1, 100)
+          "R" -> Integer.mod(pos + 1, 100)
+        end
+
+      new_count = if new_pos == 0, do: count + 1, else: count
+
+      {new_pos, new_count}
+    end)
   end
 end
 
